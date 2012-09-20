c0b3bd345b85447c83d287095146ef4e941f23c3
David <david.loubere@gmail.com>
2012-09-20 23:01:36 +0200

    First import of the demos folder (including the facade demo)

---
+++ demos/demo-facade.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Demo facade
+ * 
+ * For this demo to work, you need to create a database: curl -X PUT http://127.0.0.1:5984/db_demo1
+ */
+
+// Define path to library directory
+define('LIBRARY_PATH', '../library');
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    LIBRARY_PATH, get_include_path(),
+)));
+
+// Zend loader
+require_once(LIBRARY_PATH.'/Zend/Loader.php');
+
+// Couchly autoloading
+function __autoload($class)
+{
+    $path = implode('/', explode('_', $class));
+    if (file_exists(LIBRARY_PATH.'/'.$path.'.php'))
+    {
+        require_once(LIBRARY_PATH.'/'.$path.'.php');
+    }
+}
+
+// Instantiate a Couchly facade
+$couchlyFacade = new Couchly_Facade('db_demo1');
+
+try
+{
+    /*
+     * Create documents
+     */
+    
+    // Create a first document
+    $id1 = uniqid();
+    $couchlyFacade->save($id1, array(
+        'firstname' => 'John',
+        'lastname' => 'Doe',
+        'created' => time()
+    ));
+    echo "Document created (id=$id1)<br><br>";
+    
+    // Create a second document
+    $id2 = uniqid();
+    $couchlyFacade->save($id2, array(
+        'firstname' => 'Foo',
+        'lastname' => 'Bar',
+        'created' => time()
+    ));
+    echo "Document created (id=$id2)<br><br>";
+
+    
+    /*
+     * Fetch documents
+     */
+    
+    echo "List of documents data<br>";
+    $result = $couchlyFacade->fetch();
+    if (isset($result->total_rows) && $result->total_rows > 0)
+    {
+        foreach ($result->rows as $document)
+        {
+            echo "<pre>".$document->id.' - '.$document->value->firstname.' '.$document->value->lastname."</pre>";
+        }
+    }
+    echo "<br>";
+    
+    
+    /*
+     * Retrieve and delete documents
+     */
+    
+    $ids = array($id1, $id2);
+    foreach ($ids as $id)
+    {
+        // Retrieve the document
+        $document = $couchlyFacade->retrieve($id);
+        if (is_null($document))
+        {
+            echo "Document does not exist (id=$id)";
+        }
+        else
+        {
+            echo "Document retrieved (id=$id)";
+            echo "<pre>";
+            var_dump($document);
+            echo "</pre>";
+            
+            // Delete the document
+            $couchlyFacade->delete($id, $document->_rev);
+            echo "Document deleted (id=$id)<br>";
+        }
+        echo "<br>";
+    }
+}
+catch (Couchly_Exception $ce)
+{
+    die($ce->getMessage());
+}
+?>
